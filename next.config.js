6f8546507983eb4779a60b62d4f542c8821e0410
Tatyana <shydalys123@gmail.com>
2024-01-07 22:30:22 -0600

    added loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    loader: "custom",
+    loaderFile: "./loader.js",
+    // unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
