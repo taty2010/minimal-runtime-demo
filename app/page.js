10afd9466c285919b2e1823125ad4688800a8109
Tatyana <shydalys123@gmail.com>
2024-01-09 13:59:46 -0600

    test

--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/image.jpg"
+          src="/seahorse.jpg"
           alt="Next.js Logo"
           width={500}
           quality={100}
